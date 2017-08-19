01ba0289269a00c2040a2cf41faef5eb74650cc2
Geoff Butler <geoffbutler@users.noreply.github.com>
2017-08-19 15:07:41 +1200

    Upgrade to SDK 2.0 / netcoreapp/netstandard2.0 + update libs where applicable

--- ContactsCore/src/ContactsCore.Data/Dao/Base.cs
+++ ContactsCore/src/ContactsCore.Data/Dao/Base.cs
@@ -34,7 +34,7 @@ namespace ContactsCore.Data.Dao
         {
             builder.Entity(GetType()) // Uid: DEFAULT
                 .Property("Uid")
-                .ForNpgsqlHasDefaultValueSql("uuid_generate_v4()")
+                .HasDefaultValueSql("uuid_generate_v4()")
                 .ValueGeneratedOnAdd();
 
             builder.Entity(GetType()) // Uid: UNIQUE
@@ -43,20 +43,20 @@ namespace ContactsCore.Data.Dao
 
             builder.Entity(GetType()) // Created: DEFAULT
                 .Property("Created")
-                .ForNpgsqlHasDefaultValueSql("(now() at time zone 'utc')")
+                .HasDefaultValueSql("(now() at time zone 'utc')")
                 .ValueGeneratedOnAdd();
 
 
             builder.Entity(GetType()) // Created: DEFAULT
                 .Property("Modified")
-                .ForNpgsqlHasDefaultValueSql("(now() at time zone 'utc')")
-                .ValueGeneratedOnAdd() // ValueGeneratedOnAddOrUpdate - update not supported by npgsql
-                .Metadata.IsReadOnlyAfterSave = false; // allow updating value in SaveChanges()
+                .HasDefaultValueSql("(now() at time zone 'utc')")
+                .ValueGeneratedOnAddOrUpdate()
+                .Metadata.AfterSaveBehavior = Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Save; // allow updating value in SaveChanges()
 
 
             builder.Entity(GetType()) // Deleted: DEFAULT
                 .Property("IsDeleted")
-                .ForNpgsqlHasDefaultValue(false)
+                .HasDefaultValue(false)
                 .ValueGeneratedOnAdd();
         }
     }
