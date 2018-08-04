2eb902c19689f171e6f0385c7b3e2ba0f88c74f1
Geoff Butler <geoffbutler@users.noreply.github.com>
2018-08-04 16:24:47 +1200

    Upgrade to Core 2.1 and update deps and fix breaking changes

--- ContactsCore/src/ContactsCore.Data/Migrations/20161124043414_initial.cs
+++ ContactsCore/src/ContactsCore.Data/Migrations/20161124043414_initial.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Metadata;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ContactsCore.Data.Migrations
 {
